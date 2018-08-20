aabf62e6bb2f7f6454bf5c4d378a4bf5a0c2ecd2
APCS3rd <APCS3rd@MHS-Dreyer-11>
2018-08-20 10:43:36 -0700

    first commit to github 8/20

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,19 @@
+
+
+	//public-other code can see this
+	//static-this is a procedure that cannot be cloned, not an object
+	//void-
+		/**
+		 * Write a description of class Hello_world here.
+		 * 
+		 * @author (your name) 
+		 * @version (a version number or a date)
+		 */
+		public class Hello_world
+		{
+		    public static void main(String[] args)
+		    {
+		        System.out.println("Hello World! From ben"); //Display the string
+		    }
+		}
+
