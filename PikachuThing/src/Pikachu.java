b6575b00e7c2f971edf1589db1aed56ef9bb4e62
APCS3rd <APCS3rd@MHS-Dreyer-11>
2018-09-10 10:29:02 -0700

    9/10/2018-just need to get this done

---
+++ PikachuThing/src/Pikachu.java
@@ -0,0 +1,16 @@
+//This code will print out a pikachu thing to the console
+//@author Benjamin Kinnaman
+//@version 8/31
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		//prints to console
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t  (\\__/)");
+		System.out.println("\t\t  (o^.^)");
+		System.out.println("\t\tz(_(\")(\")");
+
+	}
+
+}
